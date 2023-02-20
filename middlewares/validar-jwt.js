612bc97ab936dcddd30c6902f9ce3fb20dacd1d8
juanfmv <juanmartzvi@gmail.com>
2023-02-20 18:35:30 +0100

    Añadir cambios para despliegue en Vercel

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -5,7 +5,7 @@ const validarJWT = (req, res = response, next) => {
   //x-token headers
   const token = req.header("x-token");
 
-  console.log(token);
+  // console.log(token);
 
   if (!token) {
     return res.status(401).json({
